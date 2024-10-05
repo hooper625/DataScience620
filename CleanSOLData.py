7583c9b12dae9ff46cf01f05bdc7af08b375a7b1
hooper625 <hooper625@gmail.com>
2024-10-05 17:33:38 -0400

    cleaned SOL Data

--- CleanSOLData.py
+++ CleanSOLData.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
+import os
+
+# Folder where you want to save the cleaned files
+output_folder = 'cleaned_SOL_files'
+
+# Create the folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # List of DataFrame names and their corresponding URLs
 dataframes = {
@@ -11,11 +19,11 @@ dataframes = {
     'Sch_PatRate_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/School_Participation_Rates_2022_2023.csv',
     'Sch_TestBy_Level_2024': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/School_Test_by_level_2023_2024.csv',
     'State_PatRate_2024': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/State-Participation_Rates_23_24.csv',
-    'State_SubReportGroup_2024': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/State-Subject_by_Reporting%20Group-_2023-2024%20(2).csv',
+    'State_SubReportGroup_2024': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/School-Subject_by_Reporting%20Group-_2023-2024.csv',
     'State_Part_Rates_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/State_Participation_Rates_2022_2023.csv',
     'State_TestBy_Level_2024': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/State_Test_by_level_2023_2024.csv',
     'Div_PassBySubj_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/division_pass_rates_by_subject_by_stu_group_2022_2023.csv',
-    'Sch_PassBySubj_2023': 'https://github.com/hooper625/DataScience620/blob/main/dataFiles/SOLData/school_pass_rates_by_subject_by_stu_group_2022_2023.csv',
+    'Sch_PassBySubj_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/school_pass_rates_by_subject_by_stu_group_2022_2023.csv',
     'Sch_PassByTest_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/school_pass_rates_by_test_2022_2023.csv',
     'State_PassBySubj_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/state_pass_rates_by_subject_by_stud_group_2022_2023.csv',
     'State_PassByTest_2023': 'https://raw.githubusercontent.com/hooper625/DataScience620/refs/heads/main/dataFiles/SOLData/state_pass_rates_by_test_2022_2023.csv',
@@ -29,11 +37,13 @@ dfs = {}
 for name, url in dataframes.items():
     # Read the CSV file into a DataFrame
     df = pd.read_csv(url)
-
-    # Replace '>', '<', and 'N' with NaN
-    df.replace({'>': np.nan, '<': np.nan, 'N': np.nan}, inplace=True)
-
     # Store the cleaned DataFrame in the dfs dictionary
     dfs[name] = df
+    # Replace '>', '<', and 'N' with NaN
+    df.replace({'>': np.nan, '<': 0, 'N': np.nan}, inplace=True)
+    #save to new file
+    dfs[name].to_csv(os.path.join(output_folder, f'clean_{name}.csv'), index=False)
 
-# Now all DataFrames are stored in the 'dfs' dictionary with the replacements made
+
+
+print(f"Saved cleaned data to clean_{name}.csv")
